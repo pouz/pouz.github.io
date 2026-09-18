2bd972982cebcab0080587e919c462634500edee
pouz <whitewyrm84@gmail.com>
2026-09-18 10:32:00 +0900

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,6 +33,6 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b17f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1789694603185"},{}]};
+_flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b17f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1789695115059"},{}]};
 
 _flutter.loader.load();
